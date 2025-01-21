23d54cf77bdbc00e10f47b613e0d54a7fbd383be
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-21 23:35:29 +0100

    done the validation

--- frontend/src/Components/Hoc/WithValidation.jsx
+++ frontend/src/Components/Hoc/WithValidation.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/display-name */
-import validator from 'validator';
+import validator, { isMobilePhone } from 'validator';
 import isEmail from 'validator/lib/isEmail';
 
 export function WithValidation(Component){
@@ -10,9 +10,7 @@ export function WithValidation(Component){
         const {nome,cognome, email, telefono, indirizzo, specializzazione} = data
       console.log(email)
         const nameSurnameRegEx = /^[A-Za-zÀ-ÿ']+([ -][A-Za-zÀ-ÿ']+)*$/;                                
-        const numberTelRegEx = /\+?\d{1,3}?[ .-]?\(?\d{2,4}\)?[ .-]?\d{3,4}[ .-]?\d{4}/
         const stringRegEx = /^[a-zA-ZàèéìòùÀÈÉÌÒÙ\s]+$/;
-        const emailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         const addressRegEx = /^([Vv]ia|[Cc]orso|[Pp]iazza|[Vv]iale|[Ll]argo|[Ss]trada|[Cc]ont[tr]ada|[Ff]razione|[Bb]orgo)\s+[A-Za-zÀ-ÿ\s']+(?:\s+\d+[A-Za-z]?)?,?\s*(\d{5}\s+)?[A-Za-zÀ-ÿ\s]+$/;
         
         function inputValidation(){
@@ -20,12 +18,12 @@ export function WithValidation(Component){
              return {valid: false , msg: 'nome non valido'}
             }
             if(!nameSurnameRegEx.test(cognome)){
-             return {valid: false , msg: 'nome non valido'}
+             return {msg: 'nome non valido'}
             }
             if(!isEmail(email)){
               return {valid: false , msg: 'email non valida'}  
             }
-            if(!numberTelRegEx.test(telefono)){
+            if(!isMobilePhone(telefono)){
                 return {valid: false , msg: 'telefono non valido'}  
             }
             if(!addressRegEx.test(indirizzo)){
