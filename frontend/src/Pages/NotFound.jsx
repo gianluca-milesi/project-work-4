292d8362647e675db8b6541cca03a3a0d08cfce8
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-01-22 12:34:01 +0100

    ADD: main to BlankLayout

--- frontend/src/Pages/NotFound.jsx
+++ frontend/src/Pages/NotFound.jsx
@@ -2,14 +2,10 @@ import { Link } from "react-router-dom"
 
 function NotFound() {
     return (
-        <main>
-            <section>
-                <div>
-                    <h1>404 <span>Pagina non trovata</span></h1>
-                    <Link to="/" >Torna alla Home</Link>
-                </div>
-            </section>
-        </main>
+        <div>
+            <h1>404 <span>Pagina non trovata</span></h1>
+            <Link to="/" >Torna alla Home</Link>
+        </div>
     )
 }
 
--- frontend/src/layouts/BlankLayout.jsx
+++ frontend/src/layouts/BlankLayout.jsx
@@ -2,9 +2,11 @@ import { Outlet } from "react-router-dom"
 
 function BlankLayout() {
     return (
-        <div>
-            <Outlet />
-        </div>
+        <main>
+            <div>
+                <Outlet />
+            </div>
+        </main>
     )
 }
 
