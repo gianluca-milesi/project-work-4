7b3a42511a96424f7bceed8b98e388cfc4e29e17
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-01-22 15:48:31 +0100

    REFactor: update Banner component structure and remove unused styles

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import {Link} from "react-router-dom";
+
+function Banner(){
+    return(
+        <div className={"container mx-auto flex justify-between items-center"} style={{backgroundColor:"var(--color-secondary)"}}>
+            <img src="http://localhost:3000/StockImg/banner_stock.png" className={`w-2/12`}/>
+            <Link to="/registration" className={"px-1.5 py-2.5 b-none rounded-md cursor-pointer font-bold uppercase text-white text-sm md:text-md lg:text-lg"} style={{backgroundColor:"var(--color-primary)"}}>
+                <h2>unisciti</h2>
+            </Link>
+            <div className={`pr-4 font-bold text-right text-sm md:text-md lg:text-lg`}>
+                <p>Sei un professionista?</p>
+                <p>Unisciti a noi</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
