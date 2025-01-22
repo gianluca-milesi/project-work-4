d9208ea40c7e0ae2090a1cb9c8bf93a116d0faf9
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-01-22 15:07:58 +0100

    ADD: implement Banner component on HomePage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,6 +1,11 @@
+import Banner from "../components/banner/Banner"
+
 function HomePage() {
     return (
-        <div>Home Page</div>
+        <div>
+            Home Page
+            <Banner></Banner>
+        </div>
     )
 }
 
