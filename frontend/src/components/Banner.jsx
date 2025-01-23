f75d1c4179f97d5f695b7fde0147a51fddd2a060
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-01-23 10:37:12 +0100

    Update Banner component to adjust padding in the container

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -4,7 +4,7 @@ const textResponsive = "text-sm md:text-lg lg:text-xl";
 
 function Banner(){
     return(
-        <div className={"container mx-auto flex justify-between items-center"} style={{backgroundColor:"var(--color-secondary)"}}>
+        <div className={"container p-0 mx-auto flex justify-between items-center"} style={{backgroundColor:"var(--color-secondary)"}}>
             <img src="http://localhost:3000/StockImg/banner_stock.png" className={`w-2/12`}/>
             <Link to="/registration" className={`${textResponsive} px-1.5 py-2.5 b-none rounded-md cursor-pointer font-bold uppercase text-white  ease-in-out duration-500 hover:scale-150`} style={{backgroundColor:"var(--color-primary)"}}>
                 <h2>unisciti</h2>
