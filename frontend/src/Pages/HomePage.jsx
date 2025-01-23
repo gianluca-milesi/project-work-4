dad2aab2b8796f24e55eea029f91fd8ceff208a8
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-01-23 10:26:35 +0100

    Home page

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,14 +1,7 @@
 
-import DoctorCard from "../components/DoctorCard/DoctorCard.jsx"
-
 function HomePage() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="sm:col-12 md:col-6"><DoctorCard /></div>
-                <div className="sm:col-12 md:col-6"><DoctorCard /></div>
-            </div>
-        </div>
+        <div>Home Page</div>
     )
 }
 
