31163f7d6bc505e3d9b689c01779854df3cabed9
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-01-23 10:11:03 +0100

    STYLE: improve text responsiveness and hover effect in Banner component

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,13 +1,15 @@
 import {Link} from "react-router-dom";
 
+const textResponsive = "text-sm md:text-lg lg:text-xl";
+
 function Banner(){
     return(
         <div className={"container mx-auto flex justify-between items-center"} style={{backgroundColor:"var(--color-secondary)"}}>
             <img src="http://localhost:3000/StockImg/banner_stock.png" className={`w-2/12`}/>
-            <Link to="/registration" className={"px-1.5 py-2.5 b-none rounded-md cursor-pointer font-bold uppercase text-white text-sm md:text-md lg:text-lg ease-in-out duration-500 hover:scale-125"} style={{backgroundColor:"var(--color-primary)"}}>
+            <Link to="/registration" className={`${textResponsive} px-1.5 py-2.5 b-none rounded-md cursor-pointer font-bold uppercase text-white  ease-in-out duration-500 hover:scale-150`} style={{backgroundColor:"var(--color-primary)"}}>
                 <h2>unisciti</h2>
             </Link>
-            <div className={`pr-4 font-bold text-right text-sm md:text-md lg:text-lg`}>
+            <div className={`${textResponsive} pr-4 font-bold text-right`}>
                 <p>Sei un professionista?</p>
                 <p>Unisciti a noi</p>
             </div>
