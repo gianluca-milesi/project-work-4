41511c94aa9f9ad46c411648fc672fdb6162cead
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-01-23 11:27:35 +0100

    ADD: DoctorCard to HomePage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import Hero from "../layouts/Hero";
-
+import DoctorCard from "../components/DoctorCard/DoctorCard";
 import Banner from "../components/Banner/Banner"
 
 function HomePage() {
     return (
         <div>
-            
+
             <Hero />
-        
+            <DoctorCard />
             <Banner></Banner>
         </div>
     )
