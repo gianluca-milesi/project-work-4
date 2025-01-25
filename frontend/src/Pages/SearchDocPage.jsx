5ac0fb9c17887cc5077126326a722cfa47386fd3
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-25 17:41:54 +0100

    add search bar

--- frontend/src/Pages/SearchDocPage.jsx
+++ frontend/src/Pages/SearchDocPage.jsx
@@ -1,6 +1,20 @@
+import Searchbar from "../Components/Searchbar";
+import { useContext } from "react";
+import GlobalContext from "../contexts/GlobalContext";
+import DoctorCard from "../components/DoctorCard/DoctorCard";
+
 function SearchDocPage() {
+    const {doctors} = useContext(GlobalContext)
     return (
-        <div>Search Doc Page</div>
+        <section> 
+            <div>Search Doc Page</div>
+            <Searchbar />
+            {doctors?doctors.map((doc, i)=>{
+                return <div key={i}>
+                    <DoctorCard item={doc}/>
+                </div>
+            }):<div>nessun dottore trovato</div>}
+        </section>   
     )
 }
 
