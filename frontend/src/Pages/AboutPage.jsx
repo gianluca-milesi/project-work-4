b0ede102f530c525b20976652afe58e9e9b2b96c
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-01-27 09:39:24 +0100

    REFACTOR: update AboutPage layout for improved responsiveness

--- frontend/src/Pages/AboutPage.jsx
+++ frontend/src/Pages/AboutPage.jsx
@@ -5,10 +5,10 @@ function AboutPage() {
     let faqs = ["Che servizi offriamo?","Come posso registrarmi?","I nostri medici sono verificati?","Posso contattare direttamente il medico?"]
     return (
         <div className="container flex justify-center my-10 align-center">
-            <div className="w-6/12">
+            <div className="hidden lg:block w-6/12">
                 <img src={aboutImg} className="w-8/12"/>
             </div>
-            <div className="w-6/12">
+            <div className="w-full lg:w-6/12">
                 <div className="mb-10">
                     <p className="text-3xl font-bold uppercase mb-[1rem]">Chi siamo</p>
                     <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum doloremque voluptatem, in, dolorem debitis illo mollitia sint dolore aspernatur animi corporis molestias nobis unde. Culpa in accusantium quisquam laudantium architecto, quas hic blanditiis assumenda ullam dignissimos ad. Officia quaerat dicta quo doloremque, consequatur ipsa itaque ex quos sequi omnis.</p>
