7058e9d1a392a20add16f1fd302aae8c30a463ac
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-01-27 11:49:06 +0100

    ADD: row cols to DoctorCard in SearchDocPage

--- frontend/src/Pages/SearchDocPage.jsx
+++ frontend/src/Pages/SearchDocPage.jsx
@@ -4,7 +4,7 @@ import GlobalContext from "../contexts/GlobalContext";
 import DoctorCard from "../components/DoctorCard/DoctorCard";
 import { useLocation } from 'react-router-dom';
 function SearchDocPage() {
-    const {doctors, setSearch} = useContext(GlobalContext)
+    const { doctors, setSearch } = useContext(GlobalContext)
     const location = useLocation();
 
     useEffect(() => {
@@ -12,21 +12,23 @@ function SearchDocPage() {
         setSearch('');
     }, [location]);
 
-    
+
     return (
-        <section className="container flex flex-wrap"> 
-        <div className="py-9 w-full flex justify-between items-center">
-            <div>Search Doc Page</div>
-            <Searchbar />
-        </div>
-        <div className="flex flex-wrap">
-            {doctors?doctors.map((doc, i)=>{
-                return <div key={i} className="w-1/4">
-                    <DoctorCard item={doc}/>
+        <section className="container flex flex-wrap">
+            <div className="py-9 w-full flex justify-between items-center">
+                <div>Search Doc Page</div>
+                <Searchbar />
+            </div>
+            <div className="container">
+                <div className="row">
+                    {doctors ? doctors.map((doc, i) => {
+                        return <div key={i} className="col-12 sm:col-6 lg:p-0">
+                            <DoctorCard item={doc} />
+                        </div>
+                    }) : <div>nessun dottore trovato</div>}
                 </div>
-            }):<div>nessun dottore trovato</div>}
-        </div>
-        </section>   
+            </div>
+        </section>
     )
 }
 
