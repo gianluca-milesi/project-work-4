6f3177b4f6fb83bd6989773d3733d6dcbf99c6de
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2025-01-28 18:16:42 +0100

    ADD restyling ditailpage, cardreview

--- frontend/src/Components/AddReviewForm.jsx
+++ frontend/src/Components/AddReviewForm.jsx
@@ -3,52 +3,70 @@ import { WithHandlerForm } from "./Hoc/WithHandlerForm";
 import { WithRegistrationForm } from "./Hoc/WithRegistration";
 import { WithValidation } from "./Hoc/WithValidation";
 
-const baseForm={
-    text:"",
-    voto:"",
-    nome:""
-}
+const baseForm = {
+  text: "",
+  voto: "",
+  nome: "",
+};
 
-export function AddReviewForm({data, handlerInput, reviewSender}){
-
-    const {text, voto, nome} = data
-    return(
-        <div className="container">
-        <form className="flex flex-col gap-4 justify-center items-center my-10" encType="multipart/form-data" onSubmit={(e)=>reviewSender(e)}>
-          <textarea
-            name="text"
-            id="textarea"
-            onChange={(e) => handlerInput(e)}
-            className="border-2 mx-5 w-3/12"
-            value={text}
-            placeholder="insercisci la tua recensione"
-          ></textarea>
-          <input
-            type="number"
-            name="voto"
-            value={voto}
-            onChange={(e) => handlerInput(e)}
-            className="border-2 mx-5 w-3/12"
-            placeholder="inserisci valutazione.."
-            min="1"
-            max="5"
-            required
-          />
+export function AddReviewForm({ data, handlerInput, reviewSender }) {
+  const { text, voto, nome } = data;
+  return (
+    <div className="container bgsecondary rounded-lg">
+      <h1 className="p-6 text-center text-2xl">Aggiungi una Recensione</h1>
+      <form
+        className="flex flex-col gap-4 justify-center items-center p-5"
+        encType="multipart/form-data"
+        onSubmit={(e) => reviewSender(e)}
+      >
+        <div className="w-full flex justify-between items-center">
           <input
             type="text"
             name="nome"
             value={nome}
             onChange={(e) => handlerInput(e)}
-            className="border-2 mx-5 w-3/12"
-            placeholder="inserisci il tuo nome..."
+            className="border-2 rounded-md p-2 w-5/12"
+            placeholder="Nome..."
             required
           />
-          <button className="bg-cyan-300 rounded-md px-2 h-10 hover:bg-cyan-600 w-3/12">invia</button>
-        </form>
-      </div>
-    )
+          <div>
+            <label htmlFor="voto" className="text-lg">
+              {" "}
+              Voto:{" "}
+            </label>
+            <select
+              name="voto"
+              value={voto}
+              onChange={(e) => handlerInput(e)}
+              className="border-2 rounded-md mx-5 p-2 text-center"
+              placeholder=""
+              required
+            >
+              {[1, 2, 3, 4, 5].map((val) => (
+                <option key={val} value={val}>
+                  {val}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <textarea
+          name="text"
+          id="textarea"
+          onChange={(e) => handlerInput(e)}
+          className="border-2 rounded-2xl p-3 mx-5 w-full"
+          value={text}
+          placeholder="insercisci la tua recensione"
+        ></textarea>
+        <button className="custom-button p-3 w-3/12">invia</button>
+      </form>
+    </div>
+  );
 }
 
-const AddReviewFinalForm = WithHandlerForm(WithValidation(WithRegistrationForm(AddReviewForm)), baseForm)
+const AddReviewFinalForm = WithHandlerForm(
+  WithValidation(WithRegistrationForm(AddReviewForm)),
+  baseForm
+);
 
-export {AddReviewFinalForm}
+export { AddReviewFinalForm };
--- frontend/src/components/ReviewCard.jsx
+++ frontend/src/components/ReviewCard.jsx
@@ -1,34 +1,35 @@
-import { Link } from "react-router-dom";
+
 import { CgProfile } from "react-icons/cg";
 
 function ReviewCard({ review = {} }) {
+  const { nome, testo, voto, id, data_inserimento } = review;
 
+  const isoDate = new Date(data_inserimento);
+  const formattedLocalDate = isoDate.toLocaleString("it-IT").replace(",", "");
 
-    const { nome, testo, voto, id, data_inserimento } = review;
-
-    const isoDate = new Date(data_inserimento);
-    const formattedLocalDate = isoDate.toLocaleString("it-IT").replace(",", "");
-
-
-    return (
-        <div className="m-2 text-white rounded-lg shadow-xl hover:shadow-2xl">
-            <div className="p-6 bg-[rgb(244,247,244)] ">
-                <h5 className="text-2xl font-semibold tracking-tight text-black flex gap-3 items-center">
-                    <CgProfile className="text-black" /> {nome}
-                </h5>
-                <div className="flex items-center gap-1 text-yellow-400 mt-2">
-                    {[...Array(voto)].map((_, index) => (
-                        <span key={index} className="text-lg">★</span>
-                    ))}
-                    {voto % 1 !== 0 && <span className="text-lg">☆</span>}
-                </div>
-                <p className="font-medium text-black mt-2 break-words">"{testo}"</p>
-            </div>
-            <div className="p-4 border-t bg-[rgb(143,211,201)] flex justify-between items-center">
-                <span className="text-sm text-gray-800">{formattedLocalDate}</span>
-            </div>
+  return (
+    <div className="m-2 text-white custom-review-card shadow-xl">
+      <div className="bg-[rgb(244,247,244)] custom-review-card">
+        <div className="p-4">
+          <h5 className="text-2xl font-semibold tracking-tight text-black flex gap-3 items-center">
+            <CgProfile className="text-black" /> {nome}
+          </h5>
+          <div className="flex items-center gap-1 text-yellow-400 mt-2">
+            {[...Array(voto)].map((_, index) => (
+              <span key={index} className="text-lg">
+                ★
+              </span>
+            ))}
+            {voto % 1 !== 0 && <span className="text-lg">☆</span>}
+          </div>
+          <p className="font-medium text-black mt-2 break-words">"{testo}"</p>
         </div>
-    );
+        <div className="p-4 custom-review-date border-t bg-[rgb(143,211,201)] flex justify-between items-center">
+          <span className="text-sm text-gray-800">{formattedLocalDate}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ReviewCard;
