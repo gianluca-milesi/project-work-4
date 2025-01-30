3d237b87b396b2507b20b83ecd29175e79ea148d
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2025-01-30 15:39:07 +0100

    ADD form email

---
+++ frontend/src/Components/FormEmail.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function EmailForm({ doctorEmail }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Invia l'email al professionista
+      await axios.post("/api/doctors/send-email", {
+        to: doctorEmail, // Email del dottore passato come prop
+        subject: "Nuovo interessato",
+        text: `Nome: ${name}\nEmail: ${email}\nTelefono: ${phone}\nMessaggio: ${message}`,
+      });
+
+      // Invia l'email di cortesia all'utente
+      await axios.post("/api/doctors/send-courtesy-email", {
+        to: email,
+        subject: "Conferma di contatto",
+        text: "Grazie per aver contattato il medico. Il medico è stato avvisato e ti contatterà a breve.",
+      });
+
+      alert("Email inviata con successo");
+    } catch (error) {
+      alert("Errore nell'invio dell'email");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Nome:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Telefono:</label>
+        <input
+          type="tel"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Messaggio:</label>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        />
+      </div>
+      <button className="custom-button" type="submit">Invia Email</button>
+    </form>
+  );
+}
+
+export default EmailForm;
