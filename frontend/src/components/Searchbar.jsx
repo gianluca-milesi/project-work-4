6c55146d67971350e530c73b939758d8503b515d
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-01-31 11:47:56 +0100

    Updated Searchbar and SearchCocPage

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,20 +1,22 @@
 import { useContext } from "react";
 import GlobalContext from "../contexts/GlobalContext";
 
-function Searchbar(){
-    const {search, setSearch} = useContext(GlobalContext)
+function Searchbar() {
 
-    return(
-      <div>
-         <input 
-         onChange={(e)=>{setSearch(e.target.value)}} 
-         className="h-8 rounded-md border-3 p-2" 
-         type="text" 
-         value={search}
-         placeholder='Cerca'/>
-      </div>
-    )
-        
+  const { search, setSearch } = useContext(GlobalContext)
+
+
+  return (
+    <div>
+      <input
+        onChange={(e) => { setSearch(e.target.value) }}
+        className="h-8 rounded-md border-3 p-2"
+        type="text"
+        value={search}
+        placeholder='Cerca' />
+    </div>
+  )
+
 }
 
 export default Searchbar;
