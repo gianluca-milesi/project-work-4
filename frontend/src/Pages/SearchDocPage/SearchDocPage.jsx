f314a56cd874ca4c107c234689adb2078a16a408
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2025-01-31 17:19:32 +0100

    ADD new custom-button

--- frontend/src/Pages/SearchDocPage/SearchDocPage.jsx
+++ frontend/src/Pages/SearchDocPage/SearchDocPage.jsx
@@ -34,7 +34,7 @@ function SearchDocPage() {
         <div className="py-9 w-full flex justify-between items-center">
           <button
             onClick={goBack}
-            className="back_button bg-[#00c3a5] text-white rounded font-semibold py-1 px-2"
+            className="back_button custom-button font-semibold py-1 px-2"
           >
             Indietro
           </button>
