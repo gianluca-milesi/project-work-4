7d74b353632d252cb8d633914fc304addf3e56b0
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2025-02-03 11:19:47 +0100

    ADD style vari

--- frontend/src/Components/FormEmail.jsx
+++ frontend/src/Components/FormEmail.jsx
@@ -22,34 +22,37 @@ function EmailForm({ data, handlerInput, emailSender, doctorEmail }) {
         <div className="flex flex-col">
           <label className="text-gray-700 pb-2">Nome</label>
           <input
+            placeholder="Inserisci il tuo nome"
             type="text"
             name="name"
             value={name}
             onChange={(e) => handlerInput(e)}
             required
-            className="border p-1 border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600"
+            className="border p-2 border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600"
           />
         </div>
         <div className="flex flex-col my-5">
           <label className="text-gray-700 pb-2">Email</label>
           <input
+            placeholder="Inserisci la tua email"
             type="email"
             name="email"
             value={email}
             onChange={(e) => handlerInput(e)}
             required
-            className="border p-1 border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600"
+            className="border p-2 border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600"
           />
         </div>
         <div className="flex flex-col my-5">
           <label className="text-gray-700 pb-2">Oggetto</label>
           <input
+            placeholder="Inserisci l'oggetto della mail"
             type="text"
             name="subject"
             value={subject}
             onChange={(e) => handlerInput(e)}
             required
-            className="border p-1 border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600"
+            className="border p-2 border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-600"
           />
         </div>
       </div>
@@ -57,6 +60,7 @@ function EmailForm({ data, handlerInput, emailSender, doctorEmail }) {
         <div className="flex flex-col flex-wrap">
           <label className="text-gray-700 pb-3">Messaggio</label>
           <textarea
+            placeholder="Scrivi il tuo messaggio al dottore"
             name="message"
             value={message}
             onChange={(e) => handlerInput(e)}
@@ -65,7 +69,7 @@ function EmailForm({ data, handlerInput, emailSender, doctorEmail }) {
             className="border p-2 border-gray-300 rounded-xl w-full focus:outline-none focus:ring-2 focus:ring-slate-950"
           />
         </div>
-        <button className="custom-button w-full py-2 my-6" type="submit">
+        <button className="custom-button w-full !py-3 my-6" type="submit">
           Invia
         </button>
       </div>
